bfca2b9325279cd78fd712afaebacd1c2e2067c5
Marius Küng <maku@mailbox.org>
2016-10-24 16:34:12 +0200

    Fixed curry' function (05)

--- haskell/fprog/05_FunctionsII/AS_FunctionsII.hs
+++ haskell/fprog/05_FunctionsII/AS_FunctionsII.hs
@@ -6,6 +6,9 @@ add' :: (Int, Int) -> Int
 --add' :: ((Int,Int) -> Int)
 add' x = fst x + snd x
 
---curry' :: (a,b) -> c -> a -> b -> c
-curry' :: ((a,b) -> c) -> a -> b -> c
-curry' f = \a -> \b -> f(a,b)
+curry' :: ((a,b) -> c) -> (a -> b -> c)
+--          x y     f      x    y    f
+curry' f = \x -> \y -> f (x,y)
+
+-- curry' mit add' verwenden
+-- curry' add' 1 2
